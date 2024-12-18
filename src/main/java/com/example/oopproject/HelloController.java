c7ace6333f1b0f47cc2a9a1163455d8e13227af3
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2024-12-19 02:21:16 +0600

    made login page design

--- src/main/java/com/example/oopproject/HelloController.java
+++ src/main/java/com/example/oopproject/HelloController.java
@@ -4,11 +4,22 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    public javafx.scene.control.TextField usernameField;
+    public javafx.scene.control.PasswordField passwordField;
+    public javafx.scene.control.Label errorLabel;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    public  void handleLogin(){
+        System.out.println("Login");
+    }
+    @FXML
+    public  void handleSignUp(){
+        System.out.println("Signup");
     }
 }
