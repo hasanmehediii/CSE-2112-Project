fd1ead1f31bfeff325f99b7831dcc2a6eb2a9451
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2024-12-26 15:12:39 +0600

    text file

--- src/main/java/com/example/oopproject/HelloApplication.java
+++ src/main/java/com/example/oopproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 600);
-        stage.setTitle("Login Page");
+        stage.setTitle("Welcome Page");
         stage.setScene(scene);
         stage.show();
     }
